93dfb490596db0aa708bf80a60099fa052d9b3e3
Ji Hun Kim <jk2227@cornell.edu>
2017-05-09 20:47:48 -0400

    using Richie's methods, refactoring

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,34 +4,29 @@ import org.apache.spark.rdd.RDD
 
 object Main {
   def main(args: Array[String] = Array()): Unit = {
-    val filepath = "/media/rhenwood39/OS/6850_proj/efilter/resultsFirst1M.json"
+    val filepath = "data/test.json"
     val conf = new SparkConf().setAppName("test").setMaster("local")
     val sc = SparkContext.getOrCreate(conf)
 
     val tweets: RDD[Tweet] = Driver.genRDDOfTweets(filepath, sc)
     println("Count " + tweets.count())
 
-    val importantHashtagsR: RDD[String] = Driver.getImportantHashTags("MAGA", 0.005, tweets)
-    println("HashTagsR: " + importantHashtagsR.collect().toSet)
+    val users : RDD[Long] = Driver.getAllusers(tweets, sc)
+    val retweetEdges : RDD[(Long, Long)] = Driver.allConnectionsRetweet(tweets, sc)
+    val mentionEdges : RDD[(Long ,Long)] = Driver.allConnectionsMention(tweets, sc)
 
-    val importantHashtagsL: RDD[String] = Driver.getImportantHashTags("MAGA", 0.005, tweets)
-    println("HashTagsL: " + importantHashtagsL.collect().toSet)
+    val retweetGraph: Graph[String, String] = Driver.buildNetwork(users, retweetEdges)
+    val mentionGraph: Graph[String, String] = Driver.buildNetwork(users, mentionEdges)
 
-    val importantHashtags: RDD[String] = importantHashtagsL.union(importantHashtagsR).distinct()
+    Driver.writeGraphToFile(retweetGraph, "/media/rhenwood39/OS/6850_proj/efilter/retweet")
+    Driver.writeGraphToFile(mentionGraph, "/media/rhenwood39/OS/6850_proj/efilter/mention")
 
-    val importantUsers: RDD[Long] = Driver.getImportantUsers(importantHashtags, tweets, sc)
-    println("Users: " + importantUsers.count())
+    // val graph = Driver.largestConnectedComp(_graph)
+    // println("conn comp: Vertices=" + graph.vertices.count() + ", Edges=" + graph.edges.count())
 
-    val connections: RDD[(Long, Long)] = Driver.filterByUsersRT(importantUsers, tweets, sc)
-    println("RT Edges: " + connections.count())
 
-    val _graph: Graph[String, String] = Driver.buildNetwork(importantUsers, connections)
-
-    val graph = Driver.largestConnectedComp(_graph)
-    println("conn comp: Vertices=" + graph.vertices.count() + ", Edges=" + graph.edges.count())
-
-    Driver.writeVerticesToFile(graph, "/media/rhenwood39/OS/6850_proj/efilter/vertices1M")
-    Driver.writeEdgesToFile(graph, "/media/rhenwood39/OS/6850_proj/efilter/edges1M")
+    // Driver.writeVerticesToFile(graph, "/media/rhenwood39/OS/6850_proj/efilter/vertices1M")
+    // Driver.writeEdgesToFile(graph, "/media/rhenwood39/OS/6850_proj/efilter/edges1M")
 
     sc.stop()
   }
